09a7120e8c0f1c01b09c9ee23d0bf3a4f87c9d1a
Ayush <ayushrawat00112@gmail.com>
2024-11-15 00:27:02 +0530

    Frontend/Backend complete, issue in mongoAtlas/Authorization

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-10 text-slate-700'>CarsManager</h1>
+      <p className='mb-4 text-xl text-slate-600'>CarsManager is a top car listing agency dedicated to assisting clients in buying, selling, and renting cars. Our team is committed to delivering outstanding service and ensuring a seamless experience throughout the buying and selling process.</p>
+      <p className='mb-4 text-xl text-slate-600'>
+      Our objective is to guide our clients in reaching their dream goals by offering expert advice, personalized service, and in-depth knowledge of the automobile market. Whether you're looking to buy, sell, or rent a car, we're here to support you every step of the way.
+      </p>
+     
+    </div>
+  )
+}
