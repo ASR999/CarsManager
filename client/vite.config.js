c5926ca48c4bf1b8c94e11cc149712162a700763
Ayush <ayushrawat00112@gmail.com>
2024-11-15 14:00:01 +0530

    backend endpoint change

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://cars-manager-a12z.vercel.app/',
+        target: 'https://cars-manager-a12z.vercel.app',
         secure: false,
       },
     },
